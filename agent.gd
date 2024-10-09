e7e3d9307d857951e2afd2d803bab5b79e34f2dc
YohanCP <145282836+YohanCP@users.noreply.github.com>
2024-10-09 23:16:05 +0700

    Create Agent

--- agent.gd
+++ agent.gd
@@ -1,19 +1,20 @@
 extends CharacterBody3D
 
-var speed = 2
-var accel = 10
+var speed = 4.0
+var accel = speed * 1.75
 
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
+@onready var target: Marker3D = $"../MainCharacter/Marker3D"
 
 func _physics_process(delta):
 	
 	var direction = Vector3()
 	
-	nav.target_position = nav.global_position
-	
-	direction = nav.get_next_path_position() - nav.position
+	nav.target_position = target.global_position
+	direction = nav.get_next_path_position() - global_position
 	direction = direction.normalized()
 	
 	velocity = velocity.lerp(direction * speed, accel * delta)
+	
 	
 	move_and_slide()
